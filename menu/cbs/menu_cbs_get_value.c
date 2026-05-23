1cffa543aeeb33d2eedc7bd8398fd4e9a74d9a45
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-05-24 00:23:15 +0900

    retroarch-2002-have_networking-fix.patch

--- menu/cbs/menu_cbs_get_value.c
+++ menu/cbs/menu_cbs_get_value.c
@@ -2141,8 +2141,7 @@ static int menu_cbs_init_bind_get_string_representation_compare_label(
             BIND_ACTION_GET_VALUE(cbs,
                   menu_action_setting_disp_set_label_manual_content_scan_core_name);
             break;
-#ifdef HAVE_NETWORKING
-         #ifdef HAVE_LAKKA_SWITCH
+#ifdef HAVE_LAKKA_SWITCH
          case MENU_ENUM_LABEL_GPU_PERF_MODE:
             BIND_ACTION_GET_VALUE(cbs,
                   menu_action_setting_disp_gpu_gov_mode);
@@ -2151,7 +2150,8 @@ static int menu_cbs_init_bind_get_string_representation_compare_label(
          case MENU_ENUM_LABEL_GPU_MANAGED_MAX_FREQ:
             BIND_ACTION_GET_VALUE(cbs, menu_action_gpu_managed_freq_label);
             break;
-         #endif
+#endif
+#ifdef HAVE_NETWORKING
          case MENU_ENUM_LABEL_CORE_UPDATER_ENTRY:
             BIND_ACTION_GET_VALUE(cbs,
                   menu_action_setting_disp_set_label_core_updater_entry);
